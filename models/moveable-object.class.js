0c1e6dd401f0e9047d25dcd297023feb1085dc2b
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-11-06 12:18:36 +0100

    First commit

---
+++ models/moveable-object.class.js
@@ -0,0 +1,20 @@
+class MoveableObject {
+    x = 120;
+    y = 280;
+    height = 170;
+    width = 100;
+    img;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img = path;
+    }
+
+    moveRight() {
+
+    }
+
+    moveLeft() {
+        
+    }
+}
