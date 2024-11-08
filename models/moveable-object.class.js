e65bd5c0f3161bd1b165d5f7a4c1824afde4615c
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-11-08 21:21:59 +0100

    funktion für die collisionsberechnung geschrieben

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -63,6 +63,14 @@ class MoveableObject {
     }
   }
 
+  isColliding (obj) {
+    return  (this.x + this.width) >= obj.x && this.x <= (obj.x + obj.width) && 
+            (this.y + this.offsetY + this.height) >= obj.y &&
+            (this.y + this.offsetY) <= (obj.y + obj.height) && 
+            obj.onCollisionCourse; // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
+
+}
+
   moveRight() {
     this.x += this.speed;
   }
